66834eb6bed0b66b89f77c23e7e606cd09515b9f
Jonathan Dunn <jondunn@jonathans-mbp-3.myfamily.int>
2015-07-30 15:18:06 -0600

    Labels and Buttons
    Labels, Buttons, and Images Added. Outlets set up

--- AnimalTrivia/ViewController.swift
+++ AnimalTrivia/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var cLabel: UILabel!
+    
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+    @IBOutlet weak var cButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var incorrectAImageView: UIImageView!
+    @IBOutlet weak var incorrectBImageView: UIImageView!
+    @IBOutlet weak var correctCImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +33,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func aButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func bButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func cButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func nextButtonPressed(sender: UIButton) {
+    }
+    
+    
 
 }
 
